cd6c1a71d8f5a0f230673cf9791877285c82dbb8
sunnysideal <76569109+sunnysideal@users.noreply.github.com>
2026-09-13 00:17:19 +0100

    Restore extraction import contracts

--- home_energy_manager/components/controller/app.py
+++ home_energy_manager/components/controller/app.py
@@ -21,14 +21,20 @@ from controller_export_generated import (
     export_generated_guardrail_end as _export_generated_guardrail_end,
     export_generated_topup_need as _export_generated_topup_need,
 )
-from controller_power_down import power_down_protected_soc as _power_down_protected_soc, power_down_info as _power_down_info
+from controller_power_down import (
+    power_down_protected_soc as _power_down_protected_soc,
+    power_down_info as _power_down_info,
+)
 from controller_battery import (
     soc_at as _soc_at, forecast_battery_kwh_between as _forecast_battery_kwh_between,
     planned_discharge_soc_adjustment as _planned_discharge_soc_adjustment, projected_charge_start_soc as _projected_charge_start_soc,
     latest_charge_start_for_rate as _latest_charge_start_for_rate, choose_rate_and_start as _choose_rate_and_start,
     band_factor as _band_factor, dwell as _dwell, charge_minutes as _charge_minutes, choose_rate as _choose_rate,
 )
-from controller_tariff import offpeak_from_forecast as _offpeak_from_forecast, persist_offpeak as _persist_offpeak, fallback_offpeak as _fallback_offpeak, current_active_offpeak as _current_active_offpeak
+from controller_tariff import (
+    offpeak_from_forecast as _offpeak_from_forecast, persist_offpeak as _persist_offpeak,
+    fallback_offpeak as _fallback_offpeak, current_active_offpeak as _current_active_offpeak,
+)
 from controller_plan_applier import apply_plan as _apply_plan, apply_safe_fallback as _apply_safe_fallback
 from controller_legacy_core import *  # noqa: F401,F403
 
